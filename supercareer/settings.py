76560a6c5d0a03f0f54e405db69b2f9b7384a3a0
bilal-tareq <bilaltareq2005@gmail.com>
2026-04-04 15:19:28 +0400

    Fix ALLOWED_HOSTS for Render deployment

--- supercareer/settings.py
+++ supercareer/settings.py
@@ -36,7 +36,7 @@ GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
